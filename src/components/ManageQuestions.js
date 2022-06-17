85e87b0c21b8afd8dcb1f0106609bdd182ec025e
ISHA <isharawat2142@gmail.com>
2022-06-17 14:54:55 +0530

    solved bug

--- src/components/ManageQuestions.js
+++ src/components/ManageQuestions.js
@@ -3,25 +3,33 @@ import {setState, useState} from 'react';
 import styles from '../styles/Post.module.css';
 import Axios from 'axios';
 export default function ManageQuestions(){
-    
-    
+        
     const [question, setName] = useState('')
     const [points, setPoints] = useState('0')
-
+    const [msg,setMsg]= useState("Please add a new question.");
     //adding question
+    
     const addNewQuestion = () => {
-        Axios.post('http://localhost:3001/add-question',{question,points})
+       Axios.post('http://localhost:3001/add-question',{question,url,points}).then(res => {
+        if(res.data.status=="Success"){
+            setMsg("Question Added Successfully");
+        }
+        else{
+            setMsg("Unable to add the question");
+        }
+      })     
     }
     
     return( 
         <div>
+            <h1>{msg}</h1>
         <div className="container">
-
         <label htmlFor="">Question name : </label>
         <input type="text" onChange={(e) => {setName(e.target.value)}}/><br/><br/>
+        <label htmlFor="">Question link : </label>
+        <input type="text" onChange={(e) => {setName(e.target.value)}}/><br/><br/>
         <label htmlFor="">Points: </label>
-        <input type="text" onChange={(e) => {setPoints(e.target.value)}}/><br/><br/>
-
+        <input type="number" onChange={(e) => {setPoints(e.target.value)}}/><br/><br/>
         <button onClick={addNewQuestion}>Add New Question</button>
 
     </div> 
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,8 @@
 import styles from "../styles/Questions.module.css";
 import style from "../styles/Home.module.css";
-
+import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons"
 import {useState, useEffect, setState} from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Axios from 'axios';
 
 export default function Question() {
@@ -21,6 +22,18 @@ export default function Question() {
           return <li key={key} className={styles.li} >
             <span>{val.question}</span>
             <span>{val.points}</span>
+            <button type="delete" style={{border: "none", backgroundColor: "#D8E4E8",}}>
+              <FontAwesomeIcon
+                icon={faTrash}          
+                style={{ fontSize: 15, color: "black", cursor: "pointer" }}
+              />
+            </button>
+            <button type="edit" style={{border: "none", backgroundColor: "#D8E4E8"}}>
+            <FontAwesomeIcon
+              icon={faEdit}          
+              style={{ fontSize: 15, color: "black", cursor: "pointer" }}
+            />
+          </button>
           </li>
         })
       }      
