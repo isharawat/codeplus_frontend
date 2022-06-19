04db60a1aaab5f2c9e29b7876e365614f2289a11
sachin-raghuwanshi <iec2020100@iiita.ac.in>
2022-06-19 16:14:58 +0530

    added post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,81 @@
-import styles from "../styles/Post.module.css"
 
-export default function Post(){    
-    return(
-        <div className={styles.post}>
-            <form>
-                <label >Title:</label>
-                <input ></input>
-                <label >Content:</label>
-                <input ></input>
-                <button >Submit</button>
-            </form>
-        </div>
+import { useState } from "react";
+import Axios from "axios";
+import { useEffect } from "react";
+import styles from "../styles/announce.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faUser } from "@fortawesome/free-solid-svg-icons";
+import Comment from "./Comment";
+import Pastcomments from "./Pastcomments";
+
+
+export default function Post() {
+    const initialvalues={title:"",body: "",comments:[], name:'',id:'' }
+    const [post, setPost] = useState([])
+
+    useEffect(() => {
+        Axios.get('http://localhost:3001/get-posts').then(res => {
+            //setQuestions(res.data.data.questions)
+            console.log(res.data);
+            setPost(res.data.data.posts)
+            console.log(post)
+        })
+
+    }, [])
+
+
+    return (
+        <>
+            <div>
+                <ul>{
+                    post.map((obj, key) => {
+                        console.log(obj);
+                        return <div key={key}>
+                            <div className={styles.container}>
+                                <h3>Title: {obj.title} </h3>
+                                <p>{obj.body}</p>
+                                <hr className={styles.line}></hr>
+                                <div className={styles.postedBy}>
+                                    <div>
+                                        <span style={{ marginLeft: "10px" }}>
+                                            <FontAwesomeIcon icon={faUser} className={styles.icon} />
+                                        </span>{" "}
+                                        <span style={{ marginLeft: "8px" }}>Posted by</span> {obj.name}
+                                        <span></span>
+                                        <span className={styles.time}>date&time</span>
+                                    </div>
+                                    <FontAwesomeIcon
+                                        icon={faComment}
+                                        className={styles.icon}
+                                        style={{ fontSize: 15, color: "black", cursor: "pointer" }}
+                                    />
+                                </div>
+                                <Pastcomments/>
+
+                                <Comment/>
+                            </div>
+                            {/* <div className={styles.post}>
+                <form>
+                    <label >Title:</label>
+                    <input ></input>
+                    <label >Content:</label>
+                    <input ></input>
+                    <button >Submit</button>
+                </form>
+            </div> */}
+                        </div>
+                    })
+                }
+                </ul>
+            </div>
+
+
+
+
+
+
+
+           
+        </>
     );
 }
